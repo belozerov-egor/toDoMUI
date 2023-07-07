d760036d291a75b124b02fb395eba2b19f803ffe
Egor <belaz.egor@gmail.com>
2023-07-07 23:29:49 +0300

    fix: todolist.reducer

--- src/features/TodolistsList/todolists.api.ts
+++ src/features/TodolistsList/todolists.api.ts
@@ -1,13 +1,10 @@
-import { instance, ResponseType } from "common/api/common.api";
-import {
-  AddTaskArgType,
-  GetTasksResponse,
-  TaskType,
-  TodolistType,
-  UpdateTaskModelType,
-} from "features/TodolistsList/todolists.types";
+import { instance } from "common/api/common.api";
 
-export const todolistsAPI = {
+import { UpdateDomainTaskModelType } from "features/TodolistsList/tasks.reducer";
+import { ResponseType } from "common/types/common.types";
+import { TaskPriorities, TaskStatuses } from "common/enums";
+
+export const todolistsApi = {
   getTodolists() {
     return instance.get<TodolistType[]>("todo-lists");
   },
@@ -17,19 +14,80 @@ export const todolistsAPI = {
   deleteTodolist(id: string) {
     return instance.delete<ResponseType>(`todo-lists/${id}`);
   },
-  updateTodolist(id: string, title: string) {
-    return instance.put<ResponseType>(`todo-lists/${id}`, { title: title });
+  updateTodolist(arg: UpdateTodolistTitleArgType) {
+    return instance.put<ResponseType>(`todo-lists/${arg.id}`, { title: arg.title });
   },
   getTasks(todolistId: string) {
     return instance.get<GetTasksResponse>(`todo-lists/${todolistId}/tasks`);
   },
-  deleteTask(todolistId: string, taskId: string) {
-    return instance.delete<ResponseType>(`todo-lists/${todolistId}/tasks/${taskId}`);
+  deleteTask(arg: RemoveTaskArgType) {
+    return instance.delete<ResponseType>(`todo-lists/${arg.todolistId}/tasks/${arg.taskId}`);
   },
   createTask(arg: AddTaskArgType) {
-    return instance.post<ResponseType<{ item: TaskType }>>(`todo-lists/${arg.todolistId}/tasks`, { title: arg.title });
+    return instance.post<
+      ResponseType<{
+        item: TaskType;
+      }>
+    >(`todo-lists/${arg.todolistId}/tasks`, { title: arg.title });
   },
   updateTask(todolistId: string, taskId: string, model: UpdateTaskModelType) {
     return instance.put<ResponseType<TaskType>>(`todo-lists/${todolistId}/tasks/${taskId}`, model);
   },
+};
+
+// Types
+export type TodolistType = {
+  id: string;
+  title: string;
+  addedDate: string;
+  order: number;
+};
+
+export type TaskType = {
+  description: string;
+  title: string;
+  status: TaskStatuses;
+  priority: TaskPriorities;
+  startDate: string;
+  deadline: string;
+  id: string;
+  todoListId: string;
+  order: number;
+  addedDate: string;
+};
+
+export type UpdateTaskModelType = {
+  title: string;
+  description: string;
+  status: TaskStatuses;
+  priority: TaskPriorities;
+  startDate: string;
+  deadline: string;
+};
+
+type GetTasksResponse = {
+  error: string | null;
+  totalCount: number;
+  items: TaskType[];
+};
+
+export type AddTaskArgType = {
+  title: string;
+  todolistId: string;
+};
+
+export type UpdateTaskArgType = {
+  taskId: string;
+  domainModel: UpdateDomainTaskModelType;
+  todolistId: string;
+};
+
+export type RemoveTaskArgType = {
+  todolistId: string;
+  taskId: string;
+};
+
+export type UpdateTodolistTitleArgType = {
+  id: string;
+  title: string;
 };
