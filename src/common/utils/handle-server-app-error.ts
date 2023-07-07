f1243b4bb8d03dd6e8c5fb22c37b53c1771e1799
Egor <belaz.egor@gmail.com>
2023-07-07 22:41:29 +0300

    fix: fix utils

--- src/common/utils/handle-server-app-error.ts
+++ src/common/utils/handle-server-app-error.ts
@@ -1,12 +1,21 @@
-import { ResponseType } from "common/api/common.api";
 import { Dispatch } from "redux";
 import { appActions } from "app/app.reducer";
+import { ResponseType } from "common/types/common.types";
 
-export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch) => {
-  if (data.messages.length) {
-    dispatch(appActions.setAppError({ error: data.messages[0] }));
-  } else {
-    dispatch(appActions.setAppError({ error: "Some error occurred" }));
+
+/**
+ * Обрабатывает ошибки, полученные от сервера.
+ *
+ * @template D Тип данных, возвращаемых сервером.
+ * @param {ResponseType<D>} data - Данные, полученные от сервера.
+ * @param {Dispatch} dispatch - Функция для отправки действий в Redux-хранилище.
+ * @param {boolean} [showError=true] - Флаг, указывающий, нужно ли показывать ошибку пользователю.
+ * @returns {void}
+ */
+export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch, showError: boolean = true) => {
+  if (showError) {
+    dispatch(appActions.setAppError({ error: data.messages.length ? data.messages[0] : "Some error occurred" }));
   }
+
   dispatch(appActions.setAppStatus({ status: "failed" }));
 };
