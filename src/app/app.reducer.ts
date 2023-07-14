09206ec47f4deedaf1eb7fada2ad2bc01246c208
Egor <belaz.egor@gmail.com>
2023-07-14 22:42:17 +0300

    fix: fix app-reducer add addMatcher

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { AnyAction } from "redux";
 
 const initialState = {
   status: "idle" as RequestStatusType,
@@ -16,12 +17,52 @@ const slice = createSlice({
     setAppError: (state, action: PayloadAction<{ error: string | null }>) => {
       state.error = action.payload.error;
     },
-    setAppStatus: (state, action: PayloadAction<{ status: RequestStatusType }>) => {
-      state.status = action.payload.status;
-    },
+    // setAppStatus: (state, action: PayloadAction<{ status: RequestStatusType }>) => {
+    //   state.status = action.payload.status;
+    // },
     setAppInitialized: (state, action: PayloadAction<{ isInitialized: boolean }>) => {
       state.isInitialized = action.payload.isInitialized;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addMatcher(
+        (action: AnyAction) => {
+          return action.type.endsWith("/pending");
+        },
+        (state) => {
+          state.status = "loading";
+        },
+      )
+      .addMatcher(
+        (action: AnyAction) => {
+          return action.type.endsWith("/rejected");
+        },
+        (state, action) => {
+          debugger;
+          state.status = "failed";
+
+          const { payload, error } = action;
+          if (payload) {
+            if (payload.showGlobalError) {
+              state.error = payload.data.messages.length ? payload.data.messages[0] : "Some error occurred";
+            }
+          } else {
+            state.error = error.message ? error.message : "Some error occurred";
+          }
+        },
+      )
+      .addMatcher(
+        (action: AnyAction) => {
+          return action.type.endsWith("/fulfilled");
+        },
+        (state) => {
+          state.status = "succeeded";
+        },
+      )
+      .addDefaultCase((state, action) => {
+        console.log("✅✅ action", action);
+      });
   },
 });
 
